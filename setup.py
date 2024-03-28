ed41fcbd803d5f3b2ed16b7a4c21482d78314515
shrikant9793 <shrinaths2@gmail.com>
2024-03-28 19:05:06 +0530

    added notebook

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Heart_disease_analysis",
+    version="0.0.0",
+    author="Shrikant9793",
+    author_email="shrinaths2@gmail.com",
+    packages=find_packages()
+)
